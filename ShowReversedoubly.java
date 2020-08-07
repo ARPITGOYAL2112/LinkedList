f5afe95c5115aaa30d36cdb600ab2fbf2aab1617
ARPIT GOYAL <60486624+ARPITGOYAL2112@users.noreply.github.com>
2020-08-07 23:44:07 +0530

    Create ShowReversedoubly.java

---
+++ ShowReversedoubly.java
@@ -0,0 +1,54 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        LinkedList list=new LinkedList();
+        list.push(1);
+        list.push(2);
+        list.push(5);
+        list.push(3);
+        list.push(7);
+        list.push(4);
+        list.push(10);
+        list.show();
+    }
+}
+class Node
+{
+    int data;
+    Node next,prev;
+}
+class LinkedList
+{
+    Node head,tail;
+    public void push(int x)
+    {
+        Node node=new Node();
+        node.data=x;
+        Node n=head;
+        if(head==null)
+        {
+            head=tail=node;
+            tail.next=null;
+            head.prev=null;
+        }
+        else
+        {
+            tail.next=node;
+            node.prev=tail;
+            tail=node;
+            tail.next=null;
+        }
+
+    }
+
+    public void show()
+    {
+        Node n=tail;
+        while(n!=null)
+        {
+            System.out.println(n.data);
+            n=n.prev;
+        }
+    }
+}
